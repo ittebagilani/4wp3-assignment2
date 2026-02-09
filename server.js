1e5f320966658fcd33e7687a09382883b79babe8
Itteba Gilani <ittebagilani@Ittebas-MacBook-Air.local>
2026-02-09 08:34:43 -0500

    change form to reflect tmdb api

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  console.log('✓ Redirecting from / to /app');
   res.redirect('/app');
 });
 
 app.get('/app', (req, res) => {
   const filePath = path.join(__dirname, 'app', 'index.html');
-  console.log('Attempting to serve:', filePath);
-  console.log('File exists:', fs.existsSync(filePath));
   res.sendFile(filePath);
 });
 
